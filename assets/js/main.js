7f1daa8baadef396ba3262266e4e1eca68e09f43
deepankg7 <kgdeepan7@gmail.com>
2026-01-28 00:33:35 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const headers = document.querySelectorAll(".accordion-header");
+
+  headers.forEach(header => {
+    header.addEventListener("click", () => {
+      const content = header.nextElementSibling;
+
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        document.querySelectorAll(".accordion-content")
+          .forEach(c => c.style.maxHeight = null);
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  });
+});
